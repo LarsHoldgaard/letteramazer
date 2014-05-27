5ac6bc0316f9cc624c4346cb0e784742181021e1
Lars Holdgaard <mcoroklo@gmail.com>
2014-05-27 15:42:04 +0200

    the project builds after getting the API project merged in

---
+++ src/LetterAmazer.Business.Services/Domain/OfficeProducts/OfficeProduct.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LetterAmazer.Business.Services.Domain.Letters;
+using LetterAmazer.Business.Services.Domain.Pricing;
+using LetterAmazer.Business.Services.Domain.ProductMatrix;
+
+namespace LetterAmazer.Business.Services.Domain.OfficeProducts
+{
+    /// <summary>
+    /// An office has a wide range of products to a specific area, with specific details
+    /// An office might have 10.000+ offices products
+    /// </summary>
+    public class OfficeProduct
+    {
+        public int Id { get; set; }
+        public int OfficeId { get; set; }
+        public ProductScope ProductScope { get; set; }
+
+        public int CountryId { get; set; }
+        public int ContinentId { get; set; }
+        public int ZipId { get; set; }
+        public LetterDetails LetterDetails { get; set; }
+        public List<ProductMatrix.ProductMatrixLine> ProductMatrixLines { get; set; }
+
+        public ProductMatrixReferenceType ReferenceType { get; set; }
+
+        public int OfficeProductReferenceId { get; set; }
+        public int ShippingDays { get; set; }
+
+        public bool Enabled { get; set; }
+
+        public bool Automatic { get; set; }
+        public DeliveryLabel DeliveryLabel { get; set; }
+
+        public OfficeProduct()
+        {
+            this.ProductMatrixLines = new List<ProductMatrixLine>();
+        }
+    }
+}
