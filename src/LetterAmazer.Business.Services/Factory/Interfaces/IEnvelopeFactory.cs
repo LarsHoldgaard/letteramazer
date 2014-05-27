bc93a47ca9514480016428041c9e233d13643754
Lars Holdgaard <mcoroklo@gmail.com>
2014-05-27 13:40:47 +0200

    envelope logic

---
+++ src/LetterAmazer.Business.Services/Factory/Interfaces/IEnvelopeFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LetterAmazer.Business.Services.Domain.Envelope;
+
+namespace LetterAmazer.Business.Services.Factory.Interfaces
+{
+    public interface IEnvelopeFactory
+    {
+        Envelope Create(object envelope);
+        List<Envelope> Create(List<object> envelopes);
+    }
+}
---
+++ src/LetterAmazer.Business.Services/Services/EnvelopeService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LetterAmazer.Business.Services.Domain.Envelope;
+using LetterAmazer.Business.Services.Factory.Interfaces;
+using LetterAmazer.Data.Repository.Data;
+
+namespace LetterAmazer.Business.Services.Services
+{
+    public class EnvelopeService:IEnvelopeService
+    {
+          private LetterAmazerEntities repository;
+        private IEnvelopeFactory envelopeFactory;
+
+        public EnvelopeService(LetterAmazerEntities repository, IEnvelopeFactory envelopeFactory)
+        {
+            this.envelopeFactory = envelopeFactory;
+            this.repository = repository;
+        }
+
+
+        public Envelope Create(Envelope envelope)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Envelope GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Envelope Update(Envelope envelope)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
