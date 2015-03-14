87d7c67ef1ba3e10fc8796f58dc3694ad26921cc
Benjamin <bn@benjamin-nielsen.dk>
2015-03-14 16:00:07 +0100

    started working

--- src/LetterAmazer.Data.DTO/AddressInfoDTO.cs
+++ src/LetterAmazer.Data.DTO/AddressInfoDTO.cs
@@ -10,15 +10,7 @@ namespace LetterAmazer.Data.DTO
     {
         public string Address1 { get; set; }
         public string Address2 { get; set; }
-        public string AttPerson { get; set; }
-        public string Organisation { get; set; }
-        public string City { get; set; }
-        public string Country { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Zipcode { get; set; }
-        public string VatNr { get; set; }
-        public string Co { get; set; }
+        
         public string State { get; set; }
     }
 }
--- src/LetterAmazer.Data.DTO/CustomerDTO.cs
+++ src/LetterAmazer.Data.DTO/CustomerDTO.cs
@@ -12,16 +12,5 @@ namespace LetterAmazer.Data.DTO
 
         public AddressInfoDTO CustomerInfo { get; set; }
         public string Email { get; set; }
-        public string Phone { get; set; }
-        public string Password { get; set; }
-        public DateTime DateCreated { get; set; }
-        public DateTime? DateModified { get; set; }
 
-        public DateTime? DateActivated { get; set; }
-        public decimal Credit { get; set; }
-        public decimal CreditLimit { get; set; }
-        public string ResetPasswordKey { get; set; }
-        public string RegisterKey { get; set; }
-        public string AccountStatus { get; set; }
-    }
 }
